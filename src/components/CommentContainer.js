68daa62fbc2ac1d0ce2c994e01168c86861cf23b
maxvidrine <max.vidrine@gmail.com>
2018-07-06 17:16:58 -0700

    connected frontend to backend

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,22 +1,37 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import { Comment } from 'semantic-ui-react'
 import CommentTemplate from './CommentTemplate'
 import CommentForm from './CommentForm'
 
+
 class CommentContainer extends Component {
+
+  // let jsonresponse = "[{\"model\": \"forum.post\", \"pk\": 1, \"fields\": {\"user\": 1, \"date\": \"2018-07-05T23:08:56Z\", \"body\": \"On the subject of bunny rabbits\"}}, {\"model\": \"forum.post\", \"pk\": 2, \"fields\": {\"user\": 2, \"date\": \"2018-07-05T23:09:23Z\", \"body\": \"On the subject of prayer\"}}]";
+  // JSON.parse(jsonresponse)
+  state = {
+    comments: []
+  }
+
+  componentWillMount = async() => {
+    await axios(`http://localhost:8000`)
+    .then(res => {
+      let comments = JSON.parse(res.data);
+      this.setState({ comments });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
+  }
+
   render () {
-    const comment_data = [
-      { "model": "forum.post",
-        "pk": 1,
-        "fields":
-          { "user": 1,
-            "date": "2018-07-05T23:08:56Z",
-            "body": "On the subject of bunny rabbits",
-          },
-      },
-      {"model": "forum.post", "pk": 2, "fields": {"user": 2, "date": "2018-07-05T23:09:23Z", "body": "On the subject of prayer"}}
-    ]
-    let comments = comment_data.map(comment => {
+
+    console.log(this.state.comments)
+
+    // comment_data = JSON.parse(jsonresponse)
+
+    let comments = this.state.comments.map(comment => {
       return <CommentTemplate 
         key={'comment' + comment.pk} 
         user={comment.fields.user} 
